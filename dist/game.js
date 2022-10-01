95aad64ff17524483a3f2c9e30841830ff579cbd
d00kl1 <harikrishnan_v@yahoo.com>
2022-10-01 15:19:50 -0700

    Add title and wait scenes

--- dist/game.js
+++ dist/game.js
@@ -1,42 +1,20 @@
-class GameScene extends Phaser.Scene {
-  enableDraw;
-  points;
-  images;
-  currentBrush;
+class SceneController extends Phaser.Scene {
   socket;
   otherPlayers;
   players;
 
-  constructor ()
-  {
-    super({ key: 'GameScene', active: true });
-
-    this.enableDraw = false;
-
-    this.points = new Array();
-    this.images = new Array();
-
-    this.currentBrush = 'red_color';
+  constructor() {
+    //super({ key: 'title', active: true });
+    super({ key: 'scene_controller'});
 
     this.socket = null;
 
     this.players = {};
     this.otherPlayers = {};
+  }
 
-  } 
-
-  preload ()
-  {        
-      this.load.image('background', 'assets/background.png');
-      this.load.image('red_brush', 'assets/red_color.png');
-      this.load.image('red_color', 'assets/red_color.png');
-      this.load.image('orange_color', 'assets/orange_color.png');
-      this.load.image('orange_brush', 'assets/orange_color.png');
-      this.load.image('yellow_color', 'assets/yellow_color.png');
-      this.load.image('green_color', 'assets/green_color.png');
-      this.load.image('blue_color', 'assets/blue_color.png');
-      this.load.image('purple_color', 'assets/purple_color.png');
-      this.load.image('erase', 'assets/erase.png');
+  preload() {
+    
   }
 
   addPlayer(playerInfo) {
@@ -48,14 +26,15 @@ class GameScene extends Phaser.Scene {
     this.otherPlayers[playerInfo.playerId] = playerInfo.name;
   }
 
-  create ()
-  {  
+  create() {
     let self = this;
-        
+
     self.socket = io();
     
     self.socket.on('initGame', function () {          
         console.log("initGame");
+
+        self.scene.start('game', {'socket': self.socket, 'players': self.players, 'otherPlayers': self.otherPlayers})
       });
 
       self.socket.on('currentPlayers', function (players) {          
@@ -78,7 +57,155 @@ class GameScene extends Phaser.Scene {
         if (self.otherPlayers.hasOwnProperty(playerId)) {            
           delete self.otherPlayers[playerId]
         }          
-      });        
+      });
+    
+    self.scene.start('title', {'socket': self.socket, 'players': self.players, 'otherPlayers': self.otherPlayers})
+  }  
+}
+
+class TitleScene extends Phaser.Scene {
+  
+  socket;
+  players;
+  otherPlayers;
+
+  constructor() {
+    //super({ key: 'title', active: true });
+    super({ key: 'title'});
+
+    this.socket = null;
+    this.players = {};
+    this.otherPlayers = {};    
+  }
+
+  init(data) {
+    console.log('TitleScene::init', data);
+    this.socket = data.socket;
+    this.players = data.players;
+    this.otherPlayers = data.otherPlayers;
+  }
+
+  preload() {
+    this.load.image('title_background', 'assets/title_screen.png');
+    this.load.image('join_button', 'assets/join_button.png');
+  }
+
+  create() {
+    let self = this;
+
+    this.add.image(400, 300, 'title_background');
+    var join_button = this.add.image(400, 650, 'join_button').setInteractive();
+
+    join_button.on('pointerdown', function (pointer) {
+      this.setTint(0xff0000);
+      self.scene.start('wait', {'socket': self.socket, 'players': self.players, 'otherPlayers': self.otherPlayers})  
+  });
+
+    /*
+    setTimeout(() => {
+      this.scene.start('game')
+    }, 10000)
+    */
+  }
+}
+
+class WaitScene extends Phaser.Scene {
+  
+  socket;
+  players;
+  otherPlayers;
+
+  constructor() {
+    //super({ key: 'title', active: true });
+    super({ key: 'wait'});
+
+    this.socket = null;
+    this.players = {};
+    this.otherPlayers = {};    
+  }
+
+  init(data) {
+    console.log('WaitScene::init', data);
+    this.socket = data.socket;
+    this.players = data.players;
+    this.otherPlayers = data.otherPlayers;
+  }
+
+  preload() {
+    this.load.image('wait_background', 'assets/wait_screen.png');
+  }
+
+  create() {
+    let self = this;    
+
+    this.add.image(400, 300, 'wait_background');
+
+    self.socket.emit("getRoomNames", "world", (response1) => {
+      console.log(response1);
+      const roomId = response1[0]["roomId"];
+      console.log('Room id ' + roomId);
+
+      self.socket.emit("joinRoom", roomId, (response2) => {
+        console.log("Joined Room");
+        
+        self.socket.emit("ready");
+      });
+    });
+    
+  }
+}
+
+
+class GameScene extends Phaser.Scene {
+  enableDraw;
+  points;
+  images;
+  currentBrush;
+  socket;
+  otherPlayers;
+  players;
+
+  constructor ()
+  {
+    //super({ key: 'game', active: true });
+    super({ key: 'game'});
+
+    this.enableDraw = false;
+
+    this.points = new Array();
+    this.images = new Array();
+
+    this.currentBrush = 'red_color';
+
+    this.socket = null;
+    this.players = {};
+    this.otherPlayers = {};
+  } 
+
+  init(data) {
+    console.log('GameScene::init', data);
+    this.socket = data.socket;
+    this.players = data.players;
+    this.otherPlayers = data.otherPlayers;
+  }
+
+  preload ()
+  {        
+      this.load.image('background', 'assets/background.png');
+      this.load.image('red_brush', 'assets/red_color.png');
+      this.load.image('red_color', 'assets/red_color.png');
+      this.load.image('orange_color', 'assets/orange_color.png');
+      this.load.image('orange_brush', 'assets/orange_color.png');
+      this.load.image('yellow_color', 'assets/yellow_color.png');
+      this.load.image('green_color', 'assets/green_color.png');
+      this.load.image('blue_color', 'assets/blue_color.png');
+      this.load.image('purple_color', 'assets/purple_color.png');
+      this.load.image('erase', 'assets/erase.png');
+  }
+
+  create ()
+  {  
+    let self = this;
 
       var background = this.add.image(400, 300, 'background').setInteractive();
       var eraseSprite = this.add.image(100, 650, 'erase').setInteractive();
@@ -108,19 +235,7 @@ class GameScene extends Phaser.Scene {
       this.addColorSelector('yellow_color', 400, 650);
       this.addColorSelector('green_color', 500, 650);
       this.addColorSelector('blue_color', 600, 650);
-      this.addColorSelector('purple_color', 700, 650);        
-
-      self.socket.emit("getRoomNames", "world", (response1) => {
-        console.log(response1);
-        const roomId = response1[0]["roomId"];
-        console.log('Room id ' + roomId);
-
-        self.socket.emit("joinRoom", roomId, (response2) => {
-          console.log("Joined Room");
-          
-          self.socket.emit("ready");
-        });
-      });
+      this.addColorSelector('purple_color', 700, 650);
   }
 
   addColorSelector(name, x, y)
@@ -164,7 +279,7 @@ let config = {
     type: Phaser.AUTO,
     width: 800,
     height: 700,    
-    scene:  [GameScene]
+    scene:  [SceneController, TitleScene, WaitScene, GameScene]
 };
 
 let game = new Phaser.Game(config);
