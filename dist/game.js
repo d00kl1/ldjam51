09023d59eea7b3781bb1b4bd526f5d820c7fd68c
d00kl1 <harikrishnan_v@yahoo.com>
2022-10-01 14:15:31 -0700

    Break out into scenes

--- dist/game.js
+++ dist/game.js
@@ -1,159 +1,170 @@
-var config = {
-    type: Phaser.AUTO,
-    width: 800,
-    height: 700,
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 200 }
-        }
-    },
-    scene: {
-        preload: preload,
-        create: create,
-        update: update
-    }
-};
+class GameScene extends Phaser.Scene {
+  enableDraw;
+  points;
+  images;
+  currentBrush;
+  socket;
+  otherPlayers;
+  players;
 
-var game = new Phaser.Game(config);
-var enableDraw = false;
-
-const points = new Array();
-const images = new Array();
-
-var currentBrush = 'red_color';
-
-var socket = null;
-
-const otherPlayers = {};
-
-function preload ()
-{        
-    this.load.image('background', 'assets/background.png');
-    this.load.image('red_brush', 'assets/red_color.png');
-    this.load.image('red_color', 'assets/red_color.png');
-    this.load.image('orange_color', 'assets/orange_color.png');
-    this.load.image('orange_brush', 'assets/orange_color.png');
-    this.load.image('yellow_color', 'assets/yellow_color.png');
-    this.load.image('green_color', 'assets/green_color.png');
-    this.load.image('blue_color', 'assets/blue_color.png');
-    this.load.image('purple_color', 'assets/purple_color.png');
-    this.load.image('erase', 'assets/erase.png');
-}
-
-function addPlayer(playerInfo) {
-  console.log('addPlayer: ' + playerInfo.name);
-}
-
-function addOtherPlayers(playerInfo) {
-  console.log('addOtherPlayers: ' + playerInfo.name);      
-  otherPlayers[playerInfo.playerId] = playerInfo.name;
-}
-
-function create ()
-{      
-  socket = io();     
-  
-  socket.on('initGame', function () {          
-      console.log("initGame");
-    });
-
-    socket.on('currentPlayers', function (players) {          
-      Object.keys(players).forEach(function (id) {
-        if (players[id].playerId === self.socket.id) {
-          addPlayer(players[id]);
-        } else {
-          addOtherPlayers(players[id]);
-        }
-      });          
-    });
-
-    socket.on('addPlayer', function (playerInfo) {          
-      addOtherPlayers(playerInfo);
-    });
-    
-    socket.on('removePlayer', function (playerId) {
-      console.log("removePlayer");
-
-      if (otherPlayers.hasOwnProperty(playerId)) {            
-        delete otherPlayers[playerId]
-      }          
-    });        
-
-    var background = this.add.image(400, 300, 'background').setInteractive();
-    var eraseSprite = this.add.image(100, 650, 'erase').setInteractive();
-
-    eraseSprite.on('pointerdown', function (pointer) {
-        this.setTint(0xff0000);
-        eraseDrawing();
-    });
-
-    eraseSprite.on('pointerout', function (pointer) {
-        this.clearTint();
-    });        
-
-    background.on('pointerdown', function (pointer) {
-
-      enableDraw = true;
-
-    }, this);
-
-    background.on('pointerup', function (pointer) {
-
-      enableDraw = false;
-    }, this);
-
-    addColorSelector(this, 'red_color', 200, 650);
-    addColorSelector(this, 'orange_color', 300, 650);
-    addColorSelector(this, 'yellow_color', 400, 650);
-    addColorSelector(this, 'green_color', 500, 650);
-    addColorSelector(this, 'blue_color', 600, 650);
-    addColorSelector(this, 'purple_color', 700, 650);        
-
-    socket.emit("getRoomNames", "world", (response1) => {
-      console.log(response1);
-      const roomId = response1[0]["roomId"];
-      console.log('Room id ' + roomId);
-
-      socket.emit("joinRoom", roomId, (response2) => {
-        console.log("Joined Room");
-        
-        socket.emit("ready");
-      });
-    });
-}
-
-function addColorSelector(context, name, x, y)
-{
-  var redSprite = context.add.image(x, y, name).setInteractive();
-
-  redSprite.on('pointerdown', function (pointer) {
-      this.setTint(0xff0000);
-      //Set brush to red
-  });
-
-  redSprite.on('pointerout', function (pointer) {
-      this.clearTint();
-      currentBrush = name;
-  });
-}
-
-function update(time, delta)
-{
-  if (enableDraw)
+  constructor ()
   {
-    image = this.add.image(game.input.mousePointer.x, game.input.mousePointer.y, currentBrush);
-    points.push(game.input.mousePointer)        
-    images.push(image);              
+    super({ key: 'GameScene', active: true });
+
+    this.enableDraw = false;
+
+    this.points = new Array();
+    this.images = new Array();
+
+    this.currentBrush = 'red_color';
+
+    this.socket = null;
+
+    this.players = {};
+    this.otherPlayers = {};
+
+  } 
+
+  preload ()
+  {        
+      this.load.image('background', 'assets/background.png');
+      this.load.image('red_brush', 'assets/red_color.png');
+      this.load.image('red_color', 'assets/red_color.png');
+      this.load.image('orange_color', 'assets/orange_color.png');
+      this.load.image('orange_brush', 'assets/orange_color.png');
+      this.load.image('yellow_color', 'assets/yellow_color.png');
+      this.load.image('green_color', 'assets/green_color.png');
+      this.load.image('blue_color', 'assets/blue_color.png');
+      this.load.image('purple_color', 'assets/purple_color.png');
+      this.load.image('erase', 'assets/erase.png');
+  }
+
+  addPlayer(playerInfo) {
+    console.log('addPlayer: ' + playerInfo.name);
+  }
+
+  addOtherPlayers(playerInfo) {
+    console.log('addOtherPlayers: ' + playerInfo.name);      
+    this.otherPlayers[playerInfo.playerId] = playerInfo.name;
+  }
+
+  create ()
+  {  
+    let self = this;
+        
+    self.socket = io();
+    
+    self.socket.on('initGame', function () {          
+        console.log("initGame");
+      });
+
+      self.socket.on('currentPlayers', function (players) {          
+        Object.keys(self.players).forEach(function (id) {
+          if (self.players[id].playerId === self.socket.id) {
+            self.addPlayer(self.players[id]);
+          } else {
+            self.addOtherPlayers(self.players[id]);
+          }
+        });          
+      });
+
+      self.socket.on('addPlayer', function (playerInfo) {          
+        self.addOtherPlayers(playerInfo);
+      });
+      
+      self.socket.on('removePlayer', function (playerId) {
+        console.log("removePlayer");
+
+        if (self.otherPlayers.hasOwnProperty(playerId)) {            
+          delete self.otherPlayers[playerId]
+        }          
+      });        
+
+      var background = this.add.image(400, 300, 'background').setInteractive();
+      var eraseSprite = this.add.image(100, 650, 'erase').setInteractive();
+
+      eraseSprite.on('pointerdown', function (pointer) {
+          this.setTint(0xff0000);
+          self.eraseDrawing();
+      });
+
+      eraseSprite.on('pointerout', function (pointer) {
+          this.clearTint();
+      });        
+
+      background.on('pointerdown', function (pointer) {
+
+        self.enableDraw = true;
+
+      }, this);
+
+      background.on('pointerup', function (pointer) {
+
+        self.enableDraw = false;
+      }, this);
+
+      this.addColorSelector('red_color', 200, 650);
+      this.addColorSelector('orange_color', 300, 650);
+      this.addColorSelector('yellow_color', 400, 650);
+      this.addColorSelector('green_color', 500, 650);
+      this.addColorSelector('blue_color', 600, 650);
+      this.addColorSelector('purple_color', 700, 650);        
+
+      self.socket.emit("getRoomNames", "world", (response1) => {
+        console.log(response1);
+        const roomId = response1[0]["roomId"];
+        console.log('Room id ' + roomId);
+
+        self.socket.emit("joinRoom", roomId, (response2) => {
+          console.log("Joined Room");
+          
+          self.socket.emit("ready");
+        });
+      });
+  }
+
+  addColorSelector(name, x, y)
+  {
+    let self = this;
+    var redSprite = this.add.image(x, y, name).setInteractive();
+
+    redSprite.on('pointerdown', function (pointer) {
+        this.setTint(0xff0000);
+        //Set brush to red
+    });
+
+    redSprite.on('pointerout', function (pointer) {
+        this.clearTint();
+        self.currentBrush = name;
+    });
+  }
+
+  update(time, delta)
+  {
+    if (this.enableDraw)
+    {
+      let image = this.add.image(game.input.mousePointer.x, game.input.mousePointer.y, this.currentBrush);
+      this.points.push(game.input.mousePointer)        
+      this.images.push(image);              
+    }
+  }
+
+  eraseDrawing() {
+    this.images.forEach(destroyImage);
+    this.images.splice(0, this.images.length)
+  }
+  
+  destroyImage(image) {     
+      image.destroy();
+      image = null;     
   }
 }
 
-function eraseDrawing() {
-  images.forEach(destroyImage);
-  images.splice(0, images.length)
- }
- 
- function destroyImage(image) {     
-    image.destroy();
-    image = null;     
-}
+let config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 700,    
+    scene:  [GameScene]
+};
+
+let game = new Phaser.Game(config);
