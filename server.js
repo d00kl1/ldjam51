ed34df43663ad880e5565a35c3789b8f9d1bf160
d00kl1 <harikrishnan_v@yahoo.com>
2022-10-01 06:35:29 -0700

    Add socket.io

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
 const path = require('path');
 const express = require('express');
-
 const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
 const DIST_DIR = path.join(__dirname, '/dist');
 const HTML_FILE = path.join(DIST_DIR, 'index.html');
 
 app.use(express.static(DIST_DIR));
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(HTML_FILE);
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
 });
 
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
-
+server.listen(PORT, () => {
+  console.log('listening on *:' + PORT);
 });
