a5a95669e87c52f9d34e61947087590a78fc734c
d00kl1 <harikrishnan_v@yahoo.com>
2022-10-01 07:33:31 -0700

    Add socket.io messages

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const { uniqueNamesGenerator, adjectives, colors, animals } = require('unique-names-generator');
 const express = require('express');
 const app = express();
 const http = require('http');
@@ -14,8 +15,38 @@ app.get('/', (req, res) => {
   res.sendFile(HTML_FILE);
 });
 
+var players = {};
 io.on('connection', (socket) => {
-  console.log('a user connected');
+  console.log('user connected');
+
+  //const randomName = uniqueNamesGenerator({ dictionaries: [adjectives, colors, animals] }); // big_red_donkey
+
+  const shortName = uniqueNamesGenerator({
+    dictionaries: [adjectives, animals, colors], // colors can be omitted here as not used
+    length: 2
+  });
+
+  // create a new player and add it to our players object
+  players[socket.id] = {    
+    playerId: socket.id,
+    name: shortName    
+  };
+
+  // send the players object to the new player
+  socket.emit('currentPlayers', players);
+
+  // Introduce others to new player
+  //socket.broadcast.emit('addPlayer', players[socket.id]);
+
+  socket.on('disconnect', function () {
+    console.log('user disconnected');
+    
+    // remove this player
+    delete players[socket.id];
+
+    // Ask others to remove this player
+    //io.emit('removePlayer', socket.id);
+  });  
 });
 
 const PORT = process.env.PORT || 8080;
@@ -23,3 +54,5 @@ const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => {
   console.log('listening on *:' + PORT);
 });
+
+
